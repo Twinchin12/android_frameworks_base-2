f2e4feb4344215dfd6b836ef56538b11f8738623
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-01 17:05:55 +0800

    SystemUI: Lockscreen clock format settings [1/2]
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6899,6 +6899,12 @@ public final class Settings {
         public static final String NOTIFICATION_MATERIAL_DISMISS_BGSTYLE = "notification_material_dismiss_bgstyle";
 
         /**
+         * Lockscreen clock format
+         * @hide
+         */
+        public static final String CLOCK_USE_CUSTOM_FORMAT = "clock_use_custom_format";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
